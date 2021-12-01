8af860beb90b8cfe526f7387aa14878bc4112aba
gifdog97 <omusubset.s@gmail.com>
2021-12-01 17:50:55 +0900

    enable feature handling

--- src/create_testset.sh
+++ src/create_testset.sh
@@ -12,27 +12,33 @@
 # path to a treebank
 # should be preprocessed for English and Hebrew
 # you can download original UD treebanks from http://universaldependencies.org/, we used 2.0 version
-treebank="../data/treebanks/en-ud-train.conllu_processed"
+# treebank="../data/treebanks/en-ud-train.conllu_processed"
+# lang="English"
+
+treebank="../data/treebanks/es-ud-train.conllu"
 lang="English"
+feature="Number"
 
 # path to the training LM data, including vocab.txt, e.g.
 lm_data="../data/lm/$lang/"
 
-mkdir -p tmp/$lang
+mkdir -p tmp/$lang/$feature
 
 # extracting word forms, lemmas and morphological features
 echo "== collect paradigms =="
-#python data/collect_paradigms.py --input $treebank --output tmp/$lang/paradigms.txt --min_freq 0
+python data/collect_paradigms.py --input $treebank --output tmp/$lang/$feature/paradigms.txt --min_freq 0
 
 echo "== extract agreement constructions =="
 # extracting patterns which correspond to agreement relation
-#python syntactic_testsets/extract_dependency_patterns.py --treebank $treebank --output tmp/$lang/ --features Number --vocab $lm_data/vocab.txt --paradigms tmp/$lang/paradigms.txt
+## with vocab
+python syntactic_testsets/extract_dependency_patterns.py --treebank $treebank --output tmp/$lang/$feature --features $feature --vocab $lm_data/vocab.txt --paradigms tmp/$lang/$feature/paradigms.txt
+
+## without vocab
+# python syntactic_testsets/extract_dependency_patterns.py --treebank $treebank --output tmp/$lang/$feature --features $feature --paradigms tmp/$lang/$feature/paradigms.txt
 
 echo "== generate test set =="
 # given a list of patterns, grep corresponding instances which satisfy generation conditions 
-python syntactic_testsets/generate_nonsense.py --treebank $treebank --paradigms tmp/$lang/paradigms.txt \
-                                               --vocab $lm_data/vocab.txt --patterns tmp/$lang/patterns.txt \
-                                               --output ../data/agreement/$lang/generated \
-                                               --lm_data $lm_data   # for estimation of token probabilities from LM training data
-
-
+# python syntactic_testsets/generate_nonsense.py --treebank $treebank --paradigms tmp/$lang/paradigms.txt \
+#                                                --vocab $lm_data/vocab.txt --patterns tmp/$lang/patterns.txt \
+#                                                --output ../data/agreement/$lang/generated_by_myself \
+#                                                --lm_data $lm_data   # for estimation of token probabilities from LM training data
